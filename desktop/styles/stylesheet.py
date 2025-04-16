057aa9f81ace524ef3a7161b8d4513a72709dcb1
GentleMann2403 <Nick2403.bw@gmail.com>
2025-04-16 11:28:44 +0300

    Добавлен кастомный ползунок

--- desktop/styles/stylesheet.py
+++ desktop/styles/stylesheet.py
@@ -12,3 +12,66 @@ STYLESHEET = """
         background-color: #f0f2f5;
     }
 """
+
+CUSTOM_SCROLLBAR = """
+    QScrollArea {
+        border: none;
+    }
+    QScrollBar:vertical {
+        border: none;
+        background: #f1f1f1;
+        width: 12px;
+        border-radius: 6px;
+        margin: 0px 0px 0px 0px;
+    }
+    QScrollBar::handle:vertical {
+        background: #888;
+        min-height: 20px;
+        border-radius: 6px;
+    }
+    QScrollBar::handle:vertical:hover {
+        background: #555;
+    }
+    QScrollBar::add-line:vertical {
+        height: 0px;
+    }
+    QScrollBar::sub-line:vertical {
+        height: 0px;
+    }
+    QScrollBar:horizontal {
+        border: none;
+        background: #f1f1f1;
+        height: 12px;
+        border-radius: 6px;
+        margin: 0px 0px 0px 0px;
+    }
+    QScrollBar::handle:horizontal {
+        background: #888;
+        min-width: 20px;
+        border-radius: 6px;
+    }
+    QScrollBar::handle:horizontal:hover {
+        background: #555;
+    }
+    QScrollBar::add-line:horizontal {
+        width: 0px;
+    }
+    QScrollBar::sub-line:horizontal {
+        width: 0px;
+    }
+"""
+
+BUTTON_STYLE = """
+    QPushButton {
+        background-color: #e53935;
+        color: white;
+        padding: 6px 12px;
+        border: none;
+        border-radius: 6px;
+        font-size: 12px;
+    }
+    QPushButton:hover {
+        background-color: #c62828;
+    }
+"""
+
--- desktop/ui/content.py
+++ desktop/ui/content.py
@@ -1,8 +1,9 @@
-from PyQt5.QtWidgets import QFrame, QVBoxLayout, QHBoxLayout, QWidget, QPushButton
+from PyQt5.QtWidgets import QFrame, QVBoxLayout, QHBoxLayout, QWidget, QPushButton, QScrollArea
 from PyQt5.QtCore import Qt
 from .search_block import SearchBlock
 from .notification import NotificationBlock
 from database.db import get_notifications, clear_notifications
+from styles.stylesheet import CUSTOM_SCROLLBAR, BUTTON_STYLE
 
 
 class Content(QFrame):
@@ -10,7 +11,7 @@ class Content(QFrame):
         super().__init__()
         self.setObjectName("Content")
         layout = QVBoxLayout()
-        layout.setContentsMargins(0, 0, 15, 15)
+        layout.setContentsMargins(0, 0, 0, 15)
         layout.setSpacing(15)
 
         self.search_block = SearchBlock()
@@ -23,31 +24,25 @@ class Content(QFrame):
 
         self.clear_btn = QPushButton("Удалить все уведомления")
         self.clear_btn.setFixedHeight(28)
-        self.clear_btn.setStyleSheet("""
-            QPushButton {
-                background-color: #e53935;
-                color: white;
-                padding: 6px 12px;
-                border: none;
-                border-radius: 6px;
-                font-size: 12px;
-            }
-            QPushButton:hover {
-                background-color: #c62828;
-            }
-        """)
+        self.clear_btn.setStyleSheet(BUTTON_STYLE)
         self.clear_btn.clicked.connect(self.clear_notifications)
 
         top_controls_layout.addWidget(self.clear_btn)
         layout.addLayout(top_controls_layout)
 
+        self.scroll_area = QScrollArea()
+        self.scroll_area.setWidgetResizable(True)
+        self.scroll_area.setStyleSheet(CUSTOM_SCROLLBAR)
+
+        self.notifications_widget = QWidget()
         self.notifications_layout = QVBoxLayout()
         self.notifications_layout.setContentsMargins(30, 0, 0, 0)
         self.notifications_layout.setSpacing(10)
 
-        self.container = QWidget()
-        self.container.setLayout(self.notifications_layout)
-        layout.addWidget(self.container, alignment=Qt.AlignTop)
+        self.notifications_widget.setLayout(self.notifications_layout)
+        self.scroll_area.setWidget(self.notifications_widget)
+
+        layout.addWidget(self.scroll_area)
 
         self.setLayout(layout)
         self.load_notifications()
